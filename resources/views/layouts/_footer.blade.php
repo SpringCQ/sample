5a39c7ec5e25387163a6a14ebe49ee412ecf23a6
ChenQuan <chenquan@haascloud.cn>
2018-01-03 21:33:12 +0800

    finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+            <img class="brand-icon" src="http://www.haascloud.cn/img/fav.ico">
+            <a href="http://www.haascloud.cn/">
+                厦门分享云科技有限公司
+            </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
